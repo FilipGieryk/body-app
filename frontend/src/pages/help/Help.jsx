fbc1b815c99dc43399bb23b6da69b359c0273274
Midget <filipgcs@gmail.com>
2025-02-04 23:16:06 +0100

    place buttons depending on object size

--- frontend/src/pages/help/Help.jsx
+++ frontend/src/pages/help/Help.jsx
@@ -7,84 +7,89 @@ import "./help.css";
 import { useState } from "react";
 const Help = () => {
   const splitsInfo = {
-    // example add workouts
-    fullBody: {
-      name: "Full Body",
-      info: "A workout that targets all major muscle groups in one session.",
-      pros: "Efficient for building overall strength.",
-      cons: "Can be tiring if not managed properly.",
-      example: ["67952ff76349301c915710ea", "679530b46349301c915713df"],
-      workouts: "3-4/week.",
+    left: {
+      fullBody: {
+        name: "Full Body",
+        info: "A workout that targets all major muscle groups in one session.",
+        pros: "Efficient for building overall strength.",
+        cons: "Can be tiring if not managed properly.",
+        example: ["67952ff76349301c915710ea", "679530b46349301c915713df"],
+        workouts: "3-4/week.",
+      },
+      pushPullLegs: {
+        name: "Push Pull Legs",
+        info: "A split that divides workouts into pushing, pulling, and legs.",
+        pros: "Allows for focused muscle training.",
+        cons: "Requires more gym days.",
+        example: "Bench press, Pull-ups, Squats, etc.",
+        workouts: "6/week.",
+      },
+      upperLower: {
+        name: "Upper Lower",
+        info: "A split that targets upper body one day and lower body the next.",
+        pros: "Balanced muscle training.",
+        cons: "Requires more recovery time.",
+        example: "Bench press, Squats, etc.",
+        workouts: "4/week.",
+      },
+      bodyPart: {
+        name: "Body Part",
+        info: "Focus on one body part per session.",
+        pros: "Intense focus on specific muscles.",
+        cons: "Longer workout sessions.",
+        example: "Leg day, Chest day, etc.",
+        workouts: "5-6/week.",
+      },
     },
-    pushPullLegs: {
-      name: "Push Pull Legs",
-      info: "A split that divides workouts into pushing, pulling, and legs.",
-      pros: "Allows for focused muscle training.",
-      cons: "Requires more gym days.",
-      example: "Bench press, Pull-ups, Squats, etc.",
-      workouts: "6/week.",
-    },
-    upperLower: {
-      name: "Upper Lower",
-      info: "A split that targets upper body one day and lower body the next.",
-      pros: "Balanced muscle training.",
-      cons: "Requires more recovery time.",
-      example: "Bench press, Squats, etc.",
-      workouts: "4/week.",
-    },
-    bodyPart: {
-      name: "Body Part",
-      info: "Focus on one body part per session.",
-      pros: "Intense focus on specific muscles.",
-      cons: "Longer workout sessions.",
-      example: "Leg day, Chest day, etc.",
-      workouts: "5-6/week.",
-    },
-    repsSets: {
-      name: "Reps and Sets",
-      info: "Focus on the number of repetitions and sets for each exercise.",
-      pros: "Customizable to individual goals.",
-      cons: "Can be confusing for beginners.",
-      example: "3 sets of 10 reps.",
-      workouts: "Depends on goals.",
-    },
-  };
-  const repsInfo = {
-    gneralFitnes: {
-      name: "General Fitness",
-      info: "a",
-      pros: "a",
-      cons: "a",
-    },
-    endurance: {
-      name: "Endurance",
-      info: "a",
-      pros: "a",
-      cons: "a",
-    },
-    hypertrophy: {
-      name: "Hypertrophy",
-      info: "a",
-      pros: "a",
-      cons: "a",
-    },
-    muscleStrength: {
-      name: "Muscle Strength",
-      info: "a",
-      pros: "a",
-      cons: "a",
-    },
-    powerSingleRep: {
-      name: "Power: Single Rep",
-      info: "a",
-      pros: "a",
-      cons: "a",
-    },
-    powerMultiReps: {
-      name: "Power: Multi Reps",
-      info: "a",
-      pros: "a",
-      cons: "a",
+    right: {
+      generalFitness: {
+        name: "generalFitness",
+        info: "Focus on the number of repetitions and sets for each exercise.",
+        pros: "Customizable to individual goals.",
+        cons: "Can be confusing for beginners.",
+        example: "3 sets of 10 reps.",
+        workouts: "Depends on goals.",
+      },
+      endurance: {
+        name: "Endurance",
+        info: "Focus on the number of repetitions and sets for each exercise.",
+        pros: "Customizable to individual goals.",
+        cons: "Can be confusing for beginners.",
+        example: "3 sets of 10 reps.",
+        workouts: "Depends on goals.",
+      },
+      hypertrophy: {
+        name: "Hypertrophy",
+        info: "Focus on the number of repetitions and sets for each exercise.",
+        pros: "Customizable to individual goals.",
+        cons: "Can be confusing for beginners.",
+        example: "3 sets of 10 reps.",
+        workouts: "Depends on goals.",
+      },
+      muscleStrength: {
+        name: "MuscleStrength",
+        info: "Focus on the number of repetitions and sets for each exercise.",
+        pros: "Customizable to individual goals.",
+        cons: "Can be confusing for beginners.",
+        example: "3 sets of 10 reps.",
+        workouts: "Depends on goals.",
+      },
+      powerSingleRep: {
+        name: "Power: Single Rep",
+        info: "Focus on the number of repetitions and sets for each exercise.",
+        pros: "Customizable to individual goals.",
+        cons: "Can be confusing for beginners.",
+        example: "3 sets of 10 reps.",
+        workouts: "Depends on goals.",
+      },
+      powerMultiReps: {
+        name: "Power: Multi Reps",
+        info: "Focus on the number of repetitions and sets for each exercise.",
+        pros: "Customizable to individual goals.",
+        cons: "Can be confusing for beginners.",
+        example: "3 sets of 10 reps.",
+        workouts: "Depends on goals.",
+      },
     },
   };
   // category
@@ -101,22 +106,22 @@ const Help = () => {
   const [selectCategory, setSelectCategory] = useState("left");
   // section
   const clipPaths = {
-    left: {
-      first: "polygon(0% 0%, 50% 0%, 25% 25%, 0% 25%)",
-      second: "polygon(0% 25%, 50% 25%, 43% 50%, 0% 43%)",
-      third: "polygon(0% 43%, 50% 50%, 50% 63%, 0% 63%)",
-      fourth: "polygon(0% 66%, 20% 66%, 20% 100%, 0% 100%)",
-    },
-    right: {
-      first: "polygon(50% 50%, 100% 50%, 100% 75%, 50% 75%)",
-      second: "polygon(50% 75%, 100% 75%, 100% 100%, 50% 100%)",
-    },
+    fullBody: "inset(0 50% 75% 0)",
+    pushPullLegs: "inset(25% 50% 60% 0)",
+    upperLower: "inset(40% 50% 40% 0)",
+    bodyPart: "inset(50% 50% 20% 0)",
+    generalFitness: "polygon(50% 50%, 100% 50%, 100% 75%, 50% 75%)",
+    endurance: "polygon(50% 75%, 100% 75%, 100% 100%, 50% 100%)",
+    hypertrophy: "inset(0 0 0 0)",
+    muscleStrength: "inset(0 0 0 0)",
+    powerSingleRep: "inset(0 0 0 0)",
+    powerMultiReps: "inset(0 0 0 0)",
   };
-
-  const [selectSection, setSelectSection] = useState("first");
-
-  const [selectedSplit, setSelectedSplit] = useState(splitsInfo);
-  const [clipStage, setClipStage] = useState("leftTop");
+  const [selectSection, setSelectSection] = useState("fullBody");
+  // is necessary
+  const [selectSplit, setSelectSplit] = useState(
+    splitsInfo[selectCategory][selectSection]
+  );
 
   const [animateCircles, setAnimateCircles] = useState(false);
 
@@ -125,18 +130,15 @@ const Help = () => {
   //   error,
   //   isLoading,
   // } = useHelpWorkouts(selectedSplit.example);
-  const handleSplitClick = (split, pos) => {
-    setSelectedSplit(splitsInfo[split]);
-    setClipStage(pos);
+  const handleSectionClick = (split) => {
+    setSelectSection(split);
+    setSelectSplit(splitsInfo[selectCategory][split]);
   };
-  console.log(selectCategory);
 
-  const handleChangeCategory = (category, pos) => {
+  const handleChangeCategory = (category) => {
     setSelectCategory(category);
-    // setClipPathSide(category);
-    // setMoveX(pos);
-    // setAnimateCircles(true);
-    // setTimeout(() => setAnimateCircles(false), 500); // Reset after animation
+    setSelectSection(Object.keys(splitsInfo[category])[0]);
+    setSelectSplit(splitsInfo[category][Object.keys(splitsInfo[category])[0]]);
   };
   return (
     <div className="help-container relative after:absolute rounded-2xl [background-image:linear-gradient(243deg,_#000000_1%,_#3b0a18_40%,_#000000_100%)] after:[background-image:linear-gradient(243deg,_#000000_1%,_#3b0a18_31%,_#000000_100%)]  after:content-[''] after:bg-black after:w-full after:h-full after:blur-3xl after:z-10">
@@ -144,27 +146,27 @@ const Help = () => {
       <div className="[grid-area:top] flex gap-10 text-xl justify-between z-20">
         <p className="text-red-500 bg-white/10 rounded-2xl p-3">difficulty:</p>
         <p className="text-red-500 bg-white/10 rounded-2xl p-3">
-          workouts: {selectedSplit.workouts}
+          workouts: {selectSplit.workouts}
         </p>
         <p className="text-red-500 bg-black/20 rounded-2xl p-3">time</p>
       </div>
-      {/* context */}
+      {/* middle */}
       <div className="[grid-area:text] z-100 text-white self-stretch">
-        <h1 className="mb-20">{selectedSplit.name}</h1>
-        <p className="text-xl mb-10">{selectedSplit.info}</p>
+        <h1 className="mb-20">{selectSplit.name}</h1>
+        <p className="text-xl mb-10">{selectSplit.info}</p>
         <table className="w-full mb-30">
           <tr>
             <th className="text-xl">Pros</th>
             <th className="text-xl">Cons</th>
           </tr>
           <tr>
-            <td className="text-green-400 text-base">{selectedSplit.pros}</td>
-            <td className="text-red-400 text-base">{selectedSplit.cons}</td>
+            <td className="text-green-400 text-base">{selectSplit.pros}</td>
+            <td className="text-red-400 text-base">{selectSplit.cons}</td>
           </tr>
         </table>
-        <p>{selectedSplit.example}</p>
+        <p>{selectSplit.example}</p>
       </div>
-      {/* footer */}
+      {/* bottom */}
       <div className="flex bg-white justify-around w-120 p-1 rounded-md [grid-area:footer] z-60">
         <motion.button
           className="bg-amber-400 rounded-md w-30 h-15 hover:bg-amber-300"
@@ -198,33 +200,57 @@ const Help = () => {
       />
       {/* low opacity circle */}
       <motion.div
-        className="absolute justify-around h-[100%] w-[60%] border-1 circle-radius left-[20%] align-middle opacity-20 z-30 "
+        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-1/2 h-[90vh] w-[90vh] circle-radius border-1 opacity-20 z-30 "
         animate={{
           clipPath: clipPathCategory[selectCategory],
         }}
-        transition={{ duration: 0.5, ease: "easeInOut" }}
+        transition={{ duration: 0.5, ease: "easeOut" }}
       />
-      {/* high opacity circle*/}
-      <div className="absolute justify-around h-[100%] w-[60%] left-[20%] z-50">
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-1/2 h-[90vh] w-[90vh] z-50">
+        {/* high opacity circle*/}
         <motion.div
-          className="absolute h-[100%] w-[100%] circle-radius border-2 z-50"
-          animate={{ clipPath: clipPaths[selectCategory][selectSection] }}
+          className="absolute h-full w-full circle-radius border-2 z-50"
+          animate={{ clipPath: clipPaths[selectSection] }}
+          transition={{ duration: 1, ease: "easeOut" }}
         />
-        {Object.keys(splitsInfo).map((splitKey, index) => (
-          <motion.button
-            key={splitKey}
-            className="relative left-[-45vw] text-2xl hover:text-white"
-            // instead of 18 make smthing like 100/splitsInfo
-            style={{ top: `${18 * index}%` }}
-            onClick={() => handleChangeCategory()}
-            animate={{}}
-            transition={{ duration: 0.3, ease: "easeOut" }}
-          >
-            {index}
-            {splitsInfo[splitKey].name}
-            <span className="relative left-[6vw] text-4xl text-red-500">•</span>
-          </motion.button>
-        ))}
+        {Object.keys(splitsInfo[selectCategory]).map(
+          (splitKey, index, array) => {
+            const totalButtons = array.length;
+            const radius = 50;
+            const angleStep = (Math.PI * 0.4) / (totalButtons - 1);
+            const angle = index * angleStep - Math.PI / 0.455;
+            const xPos = radius * Math.cos(angle);
+            const yPos = radius * Math.sin(angle);
+
+            return (
+              <motion.button
+                key={splitKey.name}
+                className="absolute text-2xl hover:text-white w-40 h-40 text-white"
+                style={{
+                  transform: `translate(-50%, -50%)`,
+                  top: `calc(50% + ${yPos}%)`,
+                  left:
+                    selectCategory === "left"
+                      ? `calc(50% - ${xPos}%)`
+                      : `calc(50% + ${xPos}%)`,
+                }}
+                onClick={() => handleSectionClick(splitKey)}
+                animate={{}}
+                transition={{ duration: 0.3, ease: "easeOut" }}
+              >
+                •
+                <span
+                  className="absolute w-40 h-20 text-white"
+                  style={{
+                    left: selectCategory === "left" ? `-70%` : `70%`,
+                  }}
+                >
+                  {splitsInfo[selectCategory][splitKey].name}
+                </span>
+              </motion.button>
+            );
+          }
+        )}
       </div>
     </div>
   );
