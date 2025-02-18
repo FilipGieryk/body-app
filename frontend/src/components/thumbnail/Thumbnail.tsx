d5b87f94ae468f804ffa438caaf0fa641bc52931
Midget <filipgcs@gmail.com>
2025-02-18 15:31:10 +0100

    add bg for exercises

--- frontend/src/components/thumbnail/Thumbnail.tsx
+++ frontend/src/components/thumbnail/Thumbnail.tsx
@@ -6,7 +6,7 @@ const Thumbnail = ({ data, className }) => {
     <div className={clsx(className)}>
       <img
         src={data?.media}
-        className="w-full h-[15rem] rounded-2xl overflow-visible bg-gray-200/50 "
+        className="w-full h-[15rem] rounded-2xl overflow-visible bg-[url(./assets/bg-thumb.png)] bg-[length:100%_100%]"
       ></img>
       <div className="grid grid-cols-2 grid-rows-2">
         <h2 className="mg-0 text-3xl truncate text-start">{data?.name}</h2>
