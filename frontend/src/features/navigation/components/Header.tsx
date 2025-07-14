e549be93fbb86e7a6eac994093dc90b33b9686aa
Midget <filipgcs@gmail.com>
2025-07-14 12:29:01 +0200

    Add burger menu for mobile header
    Implemented a sliding burger menu that appears on small screens.
    It toggles visibility with icon click and enhances mobile navigation.

--- frontend/src/features/navigation/components/Header.tsx
+++ frontend/src/features/navigation/components/Header.tsx
@@ -19,7 +19,8 @@ export const Header = () => {
   const [visibleModal, setVisibleModal] = useState(null);
   const { exercises, setInitialExercises, handleDeleteExercise } = useWorkout();
   const [isBasketVisible, setIsBasketVisible] = useState(false);
-
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  console.log(isMenuOpen);
   const navigate = useNavigate();
   const location = useLocation();
   // const { data: chats } = useGetChats();
@@ -69,7 +70,48 @@ export const Header = () => {
   return (
     <>
       <header className="flex justify-end h-screen top-0 absolute right-0 z-200 w-0">
-        <nav className="flex flex-col justify-center pr-7">
+        <div
+          className="relative z-10 md:hidden"
+          onClick={() => setIsMenuOpen((prev) => !prev)}
+        >
+          <svg
+            className="z-20"
+            xmlns="http://www.w3.org/2000/svg"
+            x="0px"
+            y="0px"
+            width="100"
+            height="100"
+            viewBox="0 0 50 50"
+          >
+            <path d="M 3 9 A 1.0001 1.0001 0 1 0 3 11 L 47 11 A 1.0001 1.0001 0 1 0 47 9 L 3 9 z M 3 24 A 1.0001 1.0001 0 1 0 3 26 L 47 26 A 1.0001 1.0001 0 1 0 47 24 L 3 24 z M 3 39 A 1.0001 1.0001 0 1 0 3 41 L 47 41 A 1.0001 1.0001 0 1 0 47 39 L 3 39 z"></path>
+          </svg>
+
+          {isMenuOpen && (
+            <div className="bg-amber-200 absolute right-0 top-0 w-100 z-1 h-full">
+              {links.map((link) => (
+                <NavLink
+                  key={link.id}
+                  to={link.path}
+                  className={`flex items-center justify-center rounded-2xl bg-white h-15 w-15 relative text-xl hover:shadow-inner first:mt-8 first:mb-auto last:mb-5 nth-4:mb-auto ${
+                    location.pathname === link.path ? "shadow-inner" : ""
+                  }`}
+                  onClick={(e) => {
+                    e.preventDefault();
+                    link.action();
+                  }}
+                >
+                  {link.icon}
+                  {link.text}
+                  {link?.notification && (
+                    <span className="absolute top-1 right-1 w-2 h-2 bg-red-500 rounded-full" />
+                  )}
+                </NavLink>
+              ))}
+            </div>
+          )}
+        </div>
+
+        <nav className="hidden md:flex flex-col justify-center pr-7">
           {links.map((link) => (
             <NavLink
               key={link.id}
@@ -79,7 +121,6 @@ export const Header = () => {
               }`}
               onClick={(e) => {
                 e.preventDefault();
-                const rect = e.target.getBoundingClientRect();
                 link.action();
               }}
             >
